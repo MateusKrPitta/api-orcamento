e00da6b6177615c1b648a9d4be643fbce1756fb4
MateusKrPitta <contato.mateuspitta@gmail.com>
2026-05-12 15:09:48 -0300

    fix: support DATABASE_URL with SSL

--- config/database.ts
+++ config/database.ts
@@ -6,13 +6,19 @@ const dbConfig = defineConfig({
   connections: {
     postgres: {
       client: 'pg',
-      connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
-      },
+      connection: env.get('DATABASE_URL')
+        ? {
+            connectionString: env.get('DATABASE_URL'),
+            ssl: env.get('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
+          }
+        : {
+            host: env.get('DB_HOST'),
+            port: env.get('DB_PORT'),
+            user: env.get('DB_USER'),
+            password: env.get('DB_PASSWORD'),
+            database: env.get('DB_DATABASE'),
+            ssl: env.get('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
+          },
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
